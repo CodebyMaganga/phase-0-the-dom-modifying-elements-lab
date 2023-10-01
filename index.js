0cde99699a1bd5b7c517e8aa13a3305cc2b3bbdf
Maganga Mwambonu <codebymaganga@gmail.com>
2023-10-01 08:33:42 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Write your code here!
+const element = document.querySelector('main')
+
+if(element){
+    element.remove()
+}
+
+const newHeader = document.createElement('h1')
+
+console.log(newHeader)
+
+newHeader.textContent = "Maganga is the champion"
+newHeader.id = 'victory'
+
+console.log(newHeader.textContent)
+
